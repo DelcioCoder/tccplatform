b38682661c50c238dcba9cc0bbdf94718e6ed71a
DelcioCoder <armandodelcio40@gmail.com>
2025-04-05 14:27:38 +0100

    feat:Busca de solicitações feitas pelos estudantes

--- connections/urls.py
+++ connections/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     ConnectionRequestCreateView,
     ConnectionRequestListView,
-    ConnectionRequestResponseView
+    ConnectionRequestResponseView,
+    ConnectionRequestStudentListView
 )
 
 urlpatterns = [
     path('create/', ConnectionRequestCreateView.as_view(), name='connection-create'),
     path('advisor/requests/', ConnectionRequestListView.as_view(), name='advisor-requests'),
     path('response/<int:pk>/', ConnectionRequestResponseView.as_view(), name='connection-response'),
+    path('student/requests/', ConnectionRequestStudentListView.as_view(), name='student-requests'),
 ]
