86409b6ce474dbe287871c288ff2b877bb1073f2
DelcioCoder <armandodelcio40@gmail.com>
2025-04-17 22:00:20 +0100

    chore:Usa o config do python-decouple para ler as variaveis de ambiente definidas em .env

--- core_platform/settings.py
+++ core_platform/settings.py
@@ -117,7 +117,7 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(config('DATABASE_URL'))
 }
 
 # Password validation
